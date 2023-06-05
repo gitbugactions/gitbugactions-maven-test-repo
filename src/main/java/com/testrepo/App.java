f18d7c3137764f6d326a49699b6c83a5e2a2da8f
Nfsaavedra <nuno.saavedra@tecnico.ulisboa.pt>
2023-06-05 14:16:10 +0100

    Init repo

---
+++ src/main/java/com/testrepo/App.java
@@ -0,0 +1,9 @@
+package com.testrepo;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println(new AppMath().sum(1, 2));
+    }
+}
---
+++ src/main/java/com/testrepo/AppMath.java
@@ -0,0 +1,10 @@
+package com.testrepo;
+
+public class AppMath {
+    public AppMath() {
+    }
+
+    public int sum(int a, int b) {
+        return a - b;
+    }
+}
